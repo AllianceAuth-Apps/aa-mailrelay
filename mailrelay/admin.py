5b2505635974cc7510595bc6826fee6dd5ec4665
ErikKalkoken <erik.kalkoken@gmail.com>
2021-12-23 23:44:32 +0100

    Improve admin site

--- mailrelay/admin.py
+++ mailrelay/admin.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.db.models.functions import Lower
 
 from .models import DiscordChannel, RelayConfig
 
@@ -16,7 +18,27 @@ class RelayConfigAdmin(admin.ModelAdmin):
         for obj in queryset:
             obj.send_new_mails()
 
+    filter_horizontal = ("channels",)
+    autocomplete_fields = ["character"]
 
-@admin.register(DiscordChannel)
-class DiscordChannelAdmin(admin.ModelAdmin):
-    ...
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        """overriding this formfield to have sorted lists in the form"""
+        if db_field.name == "channels":
+            kwargs["queryset"] = DiscordChannel.objects.all().order_by(Lower("name"))
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+
+if settings.DEBUG:
+
+    @admin.register(DiscordChannel)
+    class DiscordChannelAdmin(admin.ModelAdmin):
+        list_display = ("id", "name")
+        list_display_links = None
+        search_fields = ("name",)
+        ordering = ("name",)
+
+        def has_add_permission(self, *args, **kwargs) -> bool:
+            return False
+
+        def has_change_permission(self, *args, **kwargs) -> bool:
+            return False
