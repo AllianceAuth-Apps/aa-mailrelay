28a1e223e0bb071f14fef7743b9d0215b5fe5e06
ErikKalkoken <erik.kalkoken@gmail.com>
2023-07-18 14:52:36 +0200

    Add pylint to CI

--- mailrelay/utils.py
+++ mailrelay/utils.py
@@ -5,7 +5,7 @@ from typing import List
 
 def chunks_by_lines(full_text: str, max_lengths: int) -> List[str]:
     """Converts text into chunks not exceeding max_lengths and split by newline."""
-    parts = list()
+    parts = []
     partial_text = ""
     for line in full_text.splitlines(keepends=True):
         if len(partial_text + line) > max_lengths:
